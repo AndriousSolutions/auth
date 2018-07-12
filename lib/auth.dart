90dc70281305c2459d9c79bb135c72a940649daf
Andrious <info@andrioussolutions.com>
2018-07-12 13:16:13 -0400

    renamed file auth.dart

--- lib/auth.dart
+++ lib/auth.dart
@@ -17,7 +17,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 ///
-///          Created  10 May 2018
+///           Created  10 May 2018
 ///
 
 import 'dart:async';
